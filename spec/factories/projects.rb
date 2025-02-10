ec3d27af4bdf18d61810317fa9c5dd8ea9ad55e2
Christopher Hopkins <christopherhopkins@Christophers-MacBook-Pro.local>
2025-02-10 13:21:22 +0000

    test: add a model spec for the project model

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :project do
-    
+    association :creator, factory: :user
+    title { "Sample Project" }
+    deadline { 1.week.from_now }
+    description { "This is a sample project description" }
   end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to(:creator).class_name("User").with_foreign_key("user_id") }
+    it { should have_rich_text(:description) }
+  end
+
+  describe "validations" do
+    let(:user) { create(:user) }
+    let(:subject) { build(:project, creator: user) }
+
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:deadline) }
+
+    it "is valid with a creator" do
+      expect(subject).to be_valid
+    end
+
+    it "is invalid without a creator" do
+      project = build(:project, creator: nil)
+      expect(project).not_to be_valid
+      expect(project.errors[:creator]).to include("must exist")
+    end
+  end
 end
